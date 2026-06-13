0fb1657b78354f82a56cfcccc37da4b71ecba933
jerome <jeromesauzet@yahoo.fr>
2026-06-13 17:55:41 +0200

    yes

--- public/404.php
+++ public/404.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="<?= BASE_URL ?>/public/css/404.css">
 </head>
 <body>
-    <p>
-        Oups, la page que vous recherchez n'existe pas. <a href="<?= BASE_URL ?>/index.php">Retour à l'accueil</a> 
-    </p>
+    <section class="error-container">
+        <h1 class="animated-text" speed="100">404 Not Found</h1>
+        <h2 class="animated-text animated-text--typing" speed="50" delay="1000">Oups, la page que vous recherchez n'existe pas.</h2>
+        <h2><a href="<?= BASE_URL ?>/index.php">Retour à l'accueil</a></h2>
+    </section>
+
+<script src="<?= BASE_URL ?>/public/js/404.js"></script>
 </body>
 </html>
